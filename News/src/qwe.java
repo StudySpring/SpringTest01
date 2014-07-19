62464ccaa122454fbef2d4174d6e87bc6ac57444
Администратор <@192.168.0.2>
2014-07-19 15:02:12 +0300

    first

---
+++ News/src/qwe.java
@@ -0,0 +1,25 @@
+
+public class qwe {
+	private String one;
+	private String two;
+	private String three;
+	public String getOne() {
+		return one;
+	}
+	public void setOne(String one) {
+		this.one = one;
+	}
+	public String getTwo() {
+		return two;
+	}
+	public void setTwo(String two) {
+		this.two = two;
+	}
+	public String getThree() {
+		return three;
+	}
+	public void setThree(String three) {
+		this.three = three;
+	}
+	
+}
